b86e1b6e02a09ca859913c58bf9b00acfd60949e
Marco Tedone <marco.tedone@gmail.com>
2023-04-10 21:35:01 +0200

    Added test to ensure gpt-3.5-turbo is there

--- src/test/java/io/techwings/openai/experiments/demo/OpenAiPayloadsForModelsTest.java
+++ src/test/java/io/techwings/openai/experiments/demo/OpenAiPayloadsForModelsTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.techwings.openai.experiments.demo.app.models.OpenAiResponseForModels;
 import io.techwings.openai.experiments.demo.app.models.OpenAiResponseWrapperForModels;
+import io.techwings.openai.experiments.demo.app.models.Permission;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,12 +13,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collector;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 class OpenAiPayloadsForModelsTest {
-
-
     private ObjectMapper mapper;
     @BeforeEach
     public void setup() {
@@ -30,6 +31,13 @@ class OpenAiPayloadsForModelsTest {
         validateReturnedPayload(responseWrapperForModels);
     }
 
+    @Test
+    void modelInPayloadMustContainGptTurbo() throws Exception {
+        List<OpenAiResponseForModels> modelsList = getModelsAsList();
+        assertTrue(modelsList.size() > 0);
+        long count = getCountOfGptTurbo(modelsList);
+        assertTrue(count > 0);
+    }
     @NotNull
     private static ObjectMapper prepareObjectMapper() {
         ObjectMapper mapper = new ObjectMapper();
@@ -48,6 +56,19 @@ class OpenAiPayloadsForModelsTest {
                 Arrays.asList(responseWrapperForModels.getModels());
         assertFalse(models.isEmpty());
         models.forEach(System.out::println);
+    }
+    @NotNull
+    private List<OpenAiResponseForModels> getModelsAsList() throws IOException {
+        OpenAiResponseWrapperForModels responseWrapperForModels = readPayloadInputAsJson(mapper);
+        OpenAiResponseForModels[] models = responseWrapperForModels.getModels();
+        List<OpenAiResponseForModels> modelsList = Arrays.asList(models);
+        return modelsList;
+    }
+    private static long getCountOfGptTurbo(List<OpenAiResponseForModels> modelsList) {
+        long count = modelsList.stream()
+                .filter(m -> m.getId().contains("gpt-3.5-turbo"))
+                .count();
+        return count;
     }
 
 }
