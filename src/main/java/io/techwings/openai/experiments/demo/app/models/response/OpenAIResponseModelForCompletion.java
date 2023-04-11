6a50d863391ba31aa7796e4594a35900b0eaf87b
Marco Tedone <marco.tedone@gmail.com>
2023-04-11 19:28:41 +0200

    Added test to validate the json payload coming back for completion

---
+++ src/main/java/io/techwings/openai/experiments/demo/app/models/response/OpenAIResponseModelForCompletion.java
@@ -0,0 +1,35 @@
+package io.techwings.openai.experiments.demo.app.models.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OpenAIResponseModelForCompletion {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("object")
+    private String object;
+
+    @JsonProperty("created")
+    private Long created;
+
+    @JsonProperty("model")
+    private String model;
+
+    @JsonProperty("choices")
+    private List<OpenAIChoice> choices;
+
+    @JsonProperty("usage")
+    private OpenAIUsage usage;
+
+}
