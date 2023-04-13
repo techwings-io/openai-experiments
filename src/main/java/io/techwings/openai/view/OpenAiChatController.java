d0e3be0e5b4243921a7151b8b40755a0e6b68aca
Marco Tedone <marco.tedone@gmail.com>
2023-04-13 10:35:45 +0200

    Add Controller with tests

--- src/main/java/io/techwings/openai/view/OpenAiChatController.java
+++ src/main/java/io/techwings/openai/view/OpenAiChatController.java
@@ -4,9 +4,11 @@ import io.techwings.openai.app.models.request.OpenAiChatRequest;
 import io.techwings.openai.app.models.response.OpenAiChatResponse;
 import io.techwings.openai.app.services.OpenAiChatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class OpenAiChatController {
@@ -16,6 +18,11 @@ public class OpenAiChatController {
 
     @PostMapping("/chat")
     public OpenAiChatResponse chat(@RequestBody  OpenAiChatRequest request) {
-        return chatService.makeChatRequest(request);
+        try {
+            return chatService.makeChatRequest(request);
+        } catch (OpenAiChatService.ChatEndpointInvocationError e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Open Ai chat request", e);
+        }
+
     }
 }
--- src/test/java/io/techwings/openai/integration/OpenAiChatControllerIntegrationTest.java
+++ src/test/java/io/techwings/openai/integration/OpenAiChatControllerIntegrationTest.java
@@ -2,7 +2,6 @@ package io.techwings.openai.integration;
 
 import io.techwings.openai.app.models.request.OpenAiChatRequest;
 import io.techwings.openai.app.models.response.OpenAiChatResponse;
-import io.techwings.openai.app.services.OpenAiChatService;
 import io.techwings.openai.main.OpenAiInteractionApplication;
 import io.techwings.openai.utils.OpenAiTestUtils;
 import io.techwings.openai.view.OpenAiChatController;
@@ -10,6 +9,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.server.ResponseStatusException;
 
 @SpringBootTest(classes = OpenAiInteractionApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -28,9 +28,10 @@ public class OpenAiChatControllerIntegrationTest {
     @Test
     void invalidRequestToController_throwsError() {
         OpenAiChatRequest invalidRequest = OpenAiTestUtils.makeInvalidChatRequest();
-        OpenAiChatService.ChatEndpointInvocationError thrown =
-                Assertions.assertThrows(OpenAiChatService.ChatEndpointInvocationError.class,
+        ResponseStatusException thrown =
+                Assertions.assertThrows(ResponseStatusException.class,
                         () -> controller.chat(invalidRequest));
         Assertions.assertNotNull(thrown.getCause());
+
     }
 }
