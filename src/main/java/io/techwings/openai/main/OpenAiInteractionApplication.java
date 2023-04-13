2bfed2dd086663c5dbb3030fb301e56061da431e
Marco Tedone <marco.tedone@gmail.com>
2023-04-13 10:03:10 +0200

    Refactor

--- src/main/java/io/techwings/openai/main/OpenAiInteractionApplication.java
+++ src/main/java/io/techwings/openai/main/OpenAiInteractionApplication.java
@@ -3,7 +3,7 @@ package io.techwings.openai.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "io.techwings.openai")
+@SpringBootApplication(scanBasePackages = {"io.techwings.openai.app", "io.techwings.openai.view"})
 public class OpenAiInteractionApplication {
     public static void main(String[] args) {
         SpringApplication.run(OpenAiInteractionApplication.class, args);
